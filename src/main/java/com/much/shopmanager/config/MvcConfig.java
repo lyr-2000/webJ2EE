00e1b92cbe1b041fc8f510873cbecd20bd7f7432
idea-your-father <1957476540@qq.com>
2020-06-10 18:45:25 +0800

    'shfasjfs'

---
+++ src/main/java/com/much/shopmanager/config/MvcConfig.java
@@ -0,0 +1,11 @@
+package com.much.shopmanager.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author lyr
+ * @create 2020/6/10 15:51
+ */
+@Configuration
+public class MvcConfig {
+}
--- src/main/java/com/much/shopmanager/config/TokenInterceptor.java
+++ src/main/java/com/much/shopmanager/config/TokenInterceptor.java
@@ -1,7 +1,9 @@
 package com.much.shopmanager.config;
 
 import com.much.shopmanager.common.util.JwtUtil;
-import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,14 +14,19 @@ import javax.servlet.http.HttpServletResponse;
  * @Author lyr
  * @create 2020/6/9 0:54
  */
-@Component
+@Slf4j
 public class TokenInterceptor  implements HandlerInterceptor {
+    // private final Logger log = LoggerFactory.getLogger(TokenInterceptor.class);
     @Override
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
         String token = httpServletRequest.getHeader("token");
+        System.out.println(123);
+        log.error("收到请求");
+        log.info("收到请求");
         try{
             JwtUtil.getJwtInformation(token);
         }catch (Exception ex) {
+            log.error("出现问题");
             return false;
         }
         //如果校验不通过 拒绝执行
